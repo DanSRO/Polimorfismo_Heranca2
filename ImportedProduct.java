000bbc0e86ee2eaf0647756f7f65e27873501339
daniel dantas <dan_dantas@hotmail.com>
2021-12-30 10:13:16 -0300

    Envio de Commit

---
+++ ImportedProduct.java
@@ -0,0 +1,30 @@
+package Polimorfismo2;
+
+public class ImportedProduct extends Product{
+	private Double customFee;
+		
+	public ImportedProduct() {
+	}
+	public ImportedProduct(String name, Double price, Double customFee) {
+		super(name, price);
+		this.customFee = customFee;		
+	}
+	public Double getCustomFee() {
+		return customFee;
+	}
+	public void setCustomFee(Double customFee) {
+		this.customFee = customFee;
+	}
+	public double totalPrice() {
+		return customFee+getPrice();
+	}
+	@Override
+	public String priceTag() {
+		return getName()
+				+" $ "
+				+String.format("%.2f", totalPrice())
+				+"(Customs fee: $ "
+				+String.format("%.2f", customFee)
+				+")";
+	}
+}
